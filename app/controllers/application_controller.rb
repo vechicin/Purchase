3f19c85fa5859681f5b77093491fe6f7e5db218b
Santiago Velosa <santiagovelosa@gmail.com>
2022-04-18 18:27:19 -0500

    Refactor sign-up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :role) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password, :role)
+      u.permit(:first_name, :last_name, :email, :password, :current_password)
     end
   end
 end
